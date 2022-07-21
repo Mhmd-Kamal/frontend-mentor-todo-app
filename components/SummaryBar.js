0200153214a423c94541ea8767b3656083b9fae0
Mhmd-Kamal <Mhmd-Kamal>
2022-07-21 23:49:31 +0200

    refactoring

---
+++ components/SummaryBar.js
@@ -0,0 +1,31 @@
+import { useSetRecoilState } from 'recoil';
+import { todosAtom } from '../utils/recoilState/atoms';
+
+export function SummaryBar({ todos }) {
+  const setTodos = useSetRecoilState(todosAtom);
+
+  const left = todos.filter((todo) => todo.completed === false);
+
+  const completedIDs = todos
+    .filter((todo) => todo.completed === true)
+    .map((todo) => todo._id);
+
+  async function handleDeleteCompleted() {
+    const res = await fetch('/api/todos', {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({ completedIDs }),
+    });
+
+    const data = await res.json();
+    setTodos(data.todos);
+  }
+  return (
+    <div className='flex items-center justify-between px-6 py-4 text-light-Dark-Grayish-Blue'>
+      <p className=''>{left.length} items left</p>
+      <button onClick={handleDeleteCompleted}>Clear Completed</button>
+    </div>
+  );
+}
---
+++ components/TodoItem.js
@@ -0,0 +1,74 @@
+import Image from 'next/image';
+import { useSetRecoilState } from 'recoil';
+import deleteIcon from '../public/icon-cross.svg';
+import { todosAtom } from '../utils/recoilState/atoms';
+
+export function TodoItem({ todo }) {
+  const setTodos = useSetRecoilState(todosAtom);
+
+  async function handleDelete(deletedTodo) {
+    try {
+      const res = await fetch(`/api/todos/${todo._id}`, {
+        method: 'DELETE',
+      });
+      if (res.ok)
+        setTodos((todos) =>
+          todos.filter((todo) => todo._id !== deletedTodo._id)
+        );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleCheckTodo(modifiedTodo) {
+    const res = await fetch(`/api/todos/${todo._id}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({ completed: !todo.completed }),
+    });
+
+    const data = await res.json();
+    // console.log(data);
+    if (res.ok)
+      setTodos((todos) => {
+        const index = todos.findIndex((todo) => todo._id === modifiedTodo._id);
+        return [...todos.slice(0, index), data.todo, ...todos.slice(index + 1)];
+      });
+  }
+
+  return (
+    <div className='w-full rounded-md'>
+      <div className='flex items-center gap-3 px-6 py-4'>
+        <input
+          onChange={() => handleCheckTodo(todo)}
+          checked={todo.completed}
+          className='w-5 h-5 transition-all border rounded-full appearance-none cursor-pointer border-light-Very-Light-Grayish-Blue focus:outline-none checked:gradient'
+          type='checkbox'
+          name='completed'
+          id='completed_checkbox'
+        />
+        <p
+          className={`flex-1 text-light-Very-Dark-Grayish-Blue focus:outline-none placeholder:text-xs ${
+            todo.completed === true && 'line-through text-light-Grayish-Blue'
+          }`}
+        >
+          {todo.text}
+        </p>
+
+        <button
+          onClick={() => handleDelete(todo)}
+          className='flex items-center cursor-pointer'
+        >
+          <Image
+            src={deleteIcon}
+            width={15}
+            height={15}
+            alt='todo delete icon'
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
